04cdb618d507459684a1f591920e76b7dcc949fd
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-02-28 22:24:49 -0600

    Home Screen

--- screens/WelcomeScreen.jsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from 'react-native'
-
-export const WelcomeScreen = () => {
-  return (
-    <SafeAreaView>
-      <Text>Welcome screen</Text>
-    </SafeAreaView>
-  )
-}
